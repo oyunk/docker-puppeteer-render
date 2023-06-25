05b76f1ceea7189e5f3ac682496cc6e758f6dc47
Oyunk <oyunkung@hotmail.com>
2023-06-24 22:18:49 -0400

    added galleria code for testing

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -22,35 +22,64 @@ const scrapeLogic = async (res) => {
       executablePath: process.env.NODE_ENV === 'production' ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath()
     });
 
+    //@@@@@@@ 2023-06-24: keep getting "Something went wrong while running Puppeteer :( Error: Requesting main frame too early!"
     
     try{
-      const page = await browser.newPage();
+      // const page = await browser.newPage();
 
-      await new Promise((resolve) => { setTimeout(resolve, 9000); });
+      // await new Promise((resolve) => { setTimeout(resolve, 9000); });
 
       // throw new Error("whooops!"); //testing to see if try catch works
     
-      await page.goto('https://pipandebby.com/wprm_print/8249', {waitUntil: "domcontentloaded", timeout: 90000});  //Apple Jam Pie Recipe Without Pectin
+      // await page.goto('https://pipandebby.com/wprm_print/8249', {waitUntil: "domcontentloaded", timeout: 90000});  //Apple Jam Pie Recipe Without Pectin
     
-      // Set screen size
-      await page.setViewport({width: 1080, height: 1024});
+      // // Set screen size
+      // await page.setViewport({width: 1080, height: 1024});
     
-      // Type into search box
-      // await page.type('.search-box__input', 'automate beyond recorder');
+      // // Type into search box
+      // // await page.type('.search-box__input', 'automate beyond recorder');
     
-      // Wait and click on first result
-      const searchResultSelector = '.wprm-block-text-bold';
-      // await page.waitForSelector(searchResultSelector);
-      // await page.click(searchResultSelector);
+      // // Wait and click on first result
+      // const searchResultSelector = '.wprm-block-text-bold';
+      // // await page.waitForSelector(searchResultSelector);
+      // // await page.click(searchResultSelector);
     
-      // Locate the full title with a unique string
-      const textSelector = await page.waitForSelector(searchResultSelector);
-      const fullTitle = await textSelector?.evaluate(el => el.textContent);
+      // // Locate the full title with a unique string
+      // const textSelector = await page.waitForSelector(searchResultSelector);
+      // const fullTitle = await textSelector?.evaluate(el => el.textContent);
     
-      // Print the full title
-      console.log('The title of this recipe post is "%s".', fullTitle);
+      // // Print the full title
+      // console.log('The title of this recipe post is "%s".', fullTitle);
 
-      res.send(fullTitle);  //i added based on tutorial
+      // res.send(fullTitle);  //i added based on tutorial
+
+      const [page] = await browser.pages();
+			  
+				console.log('Navigating to Galleria url ...');
+			  
+				await page.goto("https://www.galleriasm.com/Category/ProductListWithCate?Searchtext=AE05&BranchNo=002&langCode=EN&Sort=&TotalCount=40&CurrrentPage=1&Pagesize=40&MakerName=&avail=Y", {waitUntil: "domcontentloaded", timeout: 9000});
+			  
+				const divProdList = await page.waitForSelector('#divProdList');
+
+				//2023-03-04: https://stackoverflow.com/questions/53252584/screenshot-for-each-dom-node
+				const itemImg = await page.$$('.product-image');
+
+				let bakedItems = await page.evaluate(() => {
+			  
+				  	var bakedNames = Array.from(document.querySelectorAll('.item > div > .item-info > div > .item-title'));
+					var bakedPrice = Array.from(document.querySelectorAll('.item > div > .item-info > div > .item-content > div > div > span > span:first-child'));
+					
+					// return [itemNamePrice.map(el => el.textContent), itemImg.map(el => el.getAttribute('src'))];
+					return [bakedNames.map(el => el.textContent), bakedPrice.map(el => el.textContent)];
+					  
+				});
+			  
+		
+			  console.log('The titems from Galleria are "%s".', bakedItems);
+
+        res.send(bakedItems);
+			  
+
     }catch (err){
 
       console.log(`There is an error : ${err}`);
