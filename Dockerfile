005cbb1a50c570ab4e6837f70d17443b93f449f5
Oyunk <oyunkung@hotmail.com>
2023-07-01 10:47:03 -0400

    updated

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,4 @@ WORKDIR /usr/src/app
 COPY package*.json ./
 RUN npm ci
 COPY . .
-EXPOSE 3000
 CMD [ "node", "index.js" ]
--- scrapeLogic.js
+++ scrapeLogic.js
@@ -65,7 +65,7 @@ const scrapeLogic = async (res) => {
       console.log('AAA:');
 
 
-      try{
+      
 			  
             console.log('Navigating to my url ...');
 
@@ -73,14 +73,14 @@ const scrapeLogic = async (res) => {
 
             console.log('BBB:');
 
-            await page.goto("https://www.google.ca/", {waitUntil: "domcontentloaded", timeout: 30000});
+            // await page.goto("https://www.google.ca/", {waitUntil: "domcontentloaded", timeout: 30000});
 
             // await page.goto("https://www.google.ca/", {waitUntil: "networkidle0", timeout: 30000});
             
-            // await page.goto("https://www.galleriasm.com/Category/ProductListWithCate?Searchtext=AE05&BranchNo=002&langCode=EN&Sort=&TotalCount=40&CurrrentPage=1&Pagesize=40&MakerName=&avail=Y", {waitUntil: "domcontentloaded", timeout: 30000});
+            await page.goto("https://www.galleriasm.com/Category/ProductListWithCate?Searchtext=AE05&BranchNo=002&langCode=EN&Sort=&TotalCount=40&CurrrentPage=1&Pagesize=40&MakerName=&avail=Y", {waitUntil: "domcontentloaded", timeout: 30000});
 
             console.log('CCC:');
-            
+    try{
 			  
 				const divProdList = await page.waitForSelector('#divProdList');
 
